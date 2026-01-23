a2edb16d2aae70e4bea5aa89395ec12acc884460
GiSeok Kang <ime.rokz@gmail.com>
2026-01-23 17:10:56 +0900

    refactor: Token 변경과 관련된 객체 제거
    - 객체 호출 제거 및 테스트 수정

--- src/main/java/com/example/demo/application/oauth/OauthService.java
+++ src/main/java/com/example/demo/application/oauth/OauthService.java
@@ -1,6 +1,5 @@
 package com.example.demo.application.oauth;
 
-import com.example.demo.application.dto.OauthToken;
 import com.example.demo.application.dto.OauthUserInfo;
 import com.example.demo.domain.Provider;
 import com.example.demo.domain.User;
@@ -13,18 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class OauthService {
 
-    private final TokenExchangerFactory exchangerFactory;
     private final IdTokenVerifier OidcIdTokenVerifierService;
     private final UserRepository userRepository;
 
     @Transactional
-    public User getUserInfo(Provider provider, String authorizationCode) {
-        OauthToken oauthToken = exchangerFactory.get(provider).exchange(authorizationCode);
-        if (oauthToken == null || oauthToken.idToken() == null) {
-            throw new IllegalArgumentException("인가 코드를 토큰으로 교환하는데 실패했습니다. (id_token을 찾을 수 없음)");
-        }
-
-        OauthUserInfo userInfo = OidcIdTokenVerifierService.verifyAndGetUserInfo(provider, oauthToken.idToken());
+    public User getUserInfo(Provider provider, String idToken) {
+        OauthUserInfo userInfo = OidcIdTokenVerifierService.verifyAndGetUserInfo(provider, idToken);
 
         return userRepository.findByProviderAndProviderId(provider, userInfo.providerId())
                 .orElseGet(() -> userRepository.save(
