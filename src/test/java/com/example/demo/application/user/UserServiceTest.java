87194df465b12c4c115536e5b2c6586d286d64ec
GiSeok Kang <ime.rokz@gmail.com>
2026-01-26 00:39:36 +0900

    feat: 친구 추가 기능 구현

--- src/test/java/com/example/demo/application/user/UserServiceTest.java
+++ src/test/java/com/example/demo/application/user/UserServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.demo.application.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.example.demo.domain.FriendRepository;
 import com.example.demo.domain.Provider;
 import com.example.demo.domain.User;
 import com.example.demo.domain.UserRepository;
@@ -19,6 +21,9 @@ class UserServiceTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    FriendRepository friendRepository;
+
     @Test
     void 닉네임이_중복되면_안된다() {
         // given
@@ -33,5 +38,22 @@ class UserServiceTest {
         assertThatThrownBy(() -> sut.registerNickname(user2Id, nickname))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("이미 존재하는 닉네임입니다.");
+    }
+
+    @Test
+    void 초대코드를_통해_친구를_맺을_수_있다() {
+        // given
+        User user1 = new User("email1", "profile1", Provider.GOOGLE, "provider-id-1");
+        User user2 = new User("email2", "profile2", Provider.GOOGLE, "provider-id-2");
+        Long user1Id = userRepository.save(user1).getId();
+        Long user2Id = userRepository.save(user2).getId();
+        sut.registerNickname(user1Id, "test1");
+        String user2InvitationCode = sut.registerNickname(user2Id, "test2");
+
+        // when
+        sut.addFriend(user1Id, user2InvitationCode);
+
+        // then
+        assertThat(friendRepository.existsFriend(user1Id, user2Id)).isTrue();
     }
 }
