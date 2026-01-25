4cc9fb5e554d7dc27329f562d20033b027e84198
GiSeok Kang <ime.rokz@gmail.com>
2026-01-26 00:39:49 +0900

    feat: 친구 추가 api 구현

--- src/main/java/com/example/demo/infrastructure/controller/UserController.java
+++ src/main/java/com/example/demo/infrastructure/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.demo.infrastructure.controller;
 
 import com.example.demo.application.user.UserService;
+import com.example.demo.infrastructure.controller.dto.InvitationCodeWebRequest;
 import com.example.demo.infrastructure.controller.dto.InvitationCodeWebResponse;
 import com.example.demo.infrastructure.controller.dto.NicknameWebRequest;
 import com.example.demo.infrastructure.interceptor.UserId;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +18,20 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users/me/nickname")
-    public ResponseEntity<InvitationCodeWebResponse> registerNickname(@Parameter(hidden = true) @UserId Long userId,
-                                                                      @RequestBody NicknameWebRequest nickNameWebRequest) {
+    public ResponseEntity<InvitationCodeWebResponse> registerNickname(@UserId Long userId,
+                                                                      @RequestBody NicknameWebRequest nickNameWebRequest
+    ) {
         String invitationCode = userService.registerNickname(userId, nickNameWebRequest.nickname());
 
         return ResponseEntity.ok(new InvitationCodeWebResponse(invitationCode));
+    }
+
+    @PostMapping("/users/me/friends")
+    public ResponseEntity<Void> connectFriend(@UserId Long userId,
+                                               @RequestBody InvitationCodeWebRequest invitationCodeWebRequest
+    ) {
+        userService.addFriend(userId, invitationCodeWebRequest.invitationCode());
+
+        return ResponseEntity.noContent().build();
     }
 }
