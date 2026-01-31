dfae286032d274b86913643d0e7e9579c87d55bd
GiSeok Kang <ime.rokz@gmail.com>
2026-01-31 21:05:50 +0900

    feat: 회원탈퇴 기능 구현

--- src/main/java/com/example/demo/application/UserService.java
+++ src/main/java/com/example/demo/application/UserService.java
@@ -1,8 +1,11 @@
 package com.example.demo.application;
 
 import com.example.demo.application.dto.UserInfo;
+import com.example.demo.domain.RefreshTokenRepository;
 import com.example.demo.domain.User;
 import com.example.demo.domain.UserRepository;
+import java.time.Clock;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +14,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final Clock clock;
 
     @Transactional(readOnly = true)
     public UserInfo getUserInfo(Long userId) {
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
         return new UserInfo(user.getId(), user.getNickname(), user.getLevel(), user.getProfile());
+    }
+
+    @Transactional
+    public void withdrawUser(Long userId) {
+        refreshTokenRepository.deleteByUserId(userId);
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        user.withdraw(LocalDateTime.now(clock));
     }
 }
